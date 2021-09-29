e7f4b223a6fca0a725bba5dde363130e54a908eb
김재현 <robot032195@gmail.com>
2021-09-29 03:17:15 +0000

    MODIFY : formatting

--- code/load_data.py
+++ code/load_data.py
@@ -13,119 +13,149 @@ from torch.utils.data import DataLoader
 
 
 class RE_Dataset(torch.utils.data.Dataset):
-  """ Dataset 구성을 위한 class."""
-  def __init__(self, pair_dataset, labels):
-    self.pair_dataset = pair_dataset
-    self.labels = labels
+    """Dataset 구성을 위한 class."""
 
-    print('--- Pair Dataset ---')
-    print(pair_dataset)
+    def __init__(self, pair_dataset, labels):
+        self.pair_dataset = pair_dataset
+        self.labels = labels
 
-  def __getitem__(self, idx):
-    item = {key: val[idx].clone().detach() for key, val in self.pair_dataset.items()}
-    item['labels'] = torch.tensor(self.labels[idx])
-    return item
+        print("--- Pair Dataset ---")
+        print(pair_dataset)
 
-  def __len__(self):
-    return len(self.labels)
+    def __getitem__(self, idx):
+        item = {
+            key: val[idx].clone().detach() for key, val in self.pair_dataset.items()
+        }
+        item["labels"] = torch.tensor(self.labels[idx])
+        return item
 
-def preprocessing_test_dataset(dataset:pd.DataFrame):
-  '''
-  A Preprocessing function to convert original test dataset to useful one
+    def __len__(self):
+        return len(self.labels)
 
-  :param dataset (DataFrame): an original test dataset from train.csv
-  :return:
-  '''
-  subject_entity = []
-  object_entity = []
-  for i, j in zip(dataset['subject_entity'], dataset['object_entity']):
-    i = eval(i)['word']  # 비틀즈
-    j = eval(j)['word']  # 조지 해리슨
 
-    subject_entity.append(i)
-    object_entity.append(j)
-  out_dataset = pd.DataFrame({'id': dataset['id'], 'sentence': dataset['sentence'], 'subject_entity': subject_entity,
-                              'object_entity': object_entity, 'label': dataset['label']})
-  return out_dataset
+def preprocessing_test_dataset(dataset: pd.DataFrame):
+    """
+    A Preprocessing function to convert original test dataset to useful one
 
-def preprocessing_dataset(dataset:pd.DataFrame):
-  '''
-  A Preprocessing function to convert original dataset to useful one
+    :param dataset (DataFrame): an original test dataset from train.csv
+    :return:
+    """
+    subject_entity = []
+    object_entity = []
+    for i, j in zip(dataset["subject_entity"], dataset["object_entity"]):
+        i = eval(i)["word"]  # 비틀즈
+        j = eval(j)["word"]  # 조지 해리슨
 
-  :param dataset (DataFrame): an original train dataset from train.csv
-  :return:
-  '''
-  subject_entity = []
-  object_entity = []
-  for i,j in zip(dataset['subject_entity'], dataset['object_entity']):
-    i = eval(i)['word']  # 비틀즈
-    j = eval(j)['word']  # 조지 해리슨
+        subject_entity.append(i)
+        object_entity.append(j)
+    out_dataset = pd.DataFrame(
+        {
+            "id": dataset["id"],
+            "sentence": dataset["sentence"],
+            "subject_entity": subject_entity,
+            "object_entity": object_entity,
+            "label": dataset["label"],
+        }
+    )
+    return out_dataset
 
-    subject_entity.append(i)
-    object_entity.append(j)
-  out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':dataset['sentence'],'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label']})
 
-  # split dataset into train, valid
-  train_set, val_set = train_test_split(out_dataset, test_size=0.2, stratify=dataset['label'], random_state=42)
+def preprocessing_dataset(dataset: pd.DataFrame):
+    """
+    A Preprocessing function to convert original dataset to useful one
 
-  print('--- Train Set Length ---')
-  print(len(train_set))
+    :param dataset (DataFrame): an original train dataset from train.csv
+    :return:
+    """
+    subject_entity = []
+    object_entity = []
+    for i, j in zip(dataset["subject_entity"], dataset["object_entity"]):
+        i = eval(i)["word"]  # 비틀즈
+        j = eval(j)["word"]  # 조지 해리슨
 
-  print('--- Val Set Length ---')
-  print(len(val_set))
-  return train_set, val_set
+        subject_entity.append(i)
+        object_entity.append(j)
+    out_dataset = pd.DataFrame(
+        {
+            "id": dataset["id"],
+            "sentence": dataset["sentence"],
+            "subject_entity": subject_entity,
+            "object_entity": object_entity,
+            "label": dataset["label"],
+        }
+    )
 
-def load_test_data(dataset_dir:str):
-  '''
-  Load original dataset from test.csv
+    # split dataset into train, valid
+    train_set, val_set = train_test_split(
+        out_dataset, test_size=0.2, stratify=dataset["label"], random_state=42
+    )
 
-  :param dataset_dir (str): a path of test.csv
-  :return:
-  '''
-  pd_dataset = pd.read_csv(dataset_dir)
-  test_set = preprocessing_test_dataset(pd_dataset)
-  return test_set
+    print("--- Train Set Length ---")
+    print(len(train_set))
 
-def load_data(dataset_dir:str):
-  '''
-  Load original dataset from train.csv
+    print("--- Val Set Length ---")
+    print(len(val_set))
+    return train_set, val_set
 
-  :param dataset_dir (str): a path of train.csv
-  :return:
-  '''
-  pd_dataset = pd.read_csv(dataset_dir)
-  train_set, val_set = preprocessing_dataset(pd_dataset)
-  return train_set, val_set
+
+def load_test_data(dataset_dir: str):
+    """
+    Load original dataset from test.csv
+
+    :param dataset_dir (str): a path of test.csv
+    :return:
+    """
+    pd_dataset = pd.read_csv(dataset_dir)
+    test_set = preprocessing_test_dataset(pd_dataset)
+    return test_set
+
+
+def load_data(dataset_dir: str):
+    """
+    Load original dataset from train.csv
+
+    :param dataset_dir (str): a path of train.csv
+    :return:
+    """
+    pd_dataset = pd.read_csv(dataset_dir)
+    train_set, val_set = preprocessing_dataset(pd_dataset)
+    return train_set, val_set
 
 
 def tokenized_dataset(dataset, tokenizer):
-    """ tokenizer에 따라 sentence를 tokenizing 합니다."""
+    """tokenizer에 따라 sentence를 tokenizing 합니다."""
     concat_entity = []
-    for e01, e02 in zip(dataset['subject_entity'], dataset['object_entity']):
-        temp = ''
-        temp = e01 + '[SEP]' + e02
+    for e01, e02 in zip(dataset["subject_entity"], dataset["object_entity"]):
+        temp = ""
+        temp = e01 + "[SEP]" + e02
 
         # 주어 + 목적어 pair
         concat_entity.append(temp)
     tokenized_sentences = tokenizer(
         concat_entity,
-        list(dataset['sentence']),
+        list(dataset["sentence"]),
         return_tensors="pt",
         padding=True,
         truncation=True,
         max_length=256,
         # max_length=46,
         add_special_tokens=True,
-        return_token_type_ids=False
+        return_token_type_ids=False,
     )
 
-    print('--- Decode Tokenized Sentences ---')
-    print(tokenizer.decode(tokenized_sentences['input_ids'][0]))
+    print("--- Decode Tokenized Sentences ---")
+    print(tokenizer.decode(tokenized_sentences["input_ids"][0]))
     return tokenized_sentences
 
 
-def make_stratifiedkfold(raw_train: DataFrame, raw_train_classes: list, n_splits: int, shuffle: bool, seed: int):
-    """ #### raw_train dataset을 stratifiedkfold로 나눠 주는 함수
+def make_stratifiedkfold(
+    raw_train: DataFrame,
+    raw_train_classes: list,
+    n_splits: int,
+    shuffle: bool,
+    seed: int,
+):
+    """#### raw_train dataset을 stratifiedkfold로 나눠 주는 함수
     raw data -> stratify 한 fklod로 만들어줌
 
     Example:
@@ -141,16 +171,14 @@ def make_stratifiedkfold(raw_train: DataFrame, raw_train_classes: list, n_splits
         raw_train (DataFrame): origin `train.csv` dataframe
         raw_train_classes (list): raw data class(label)
         n_splits (int): fold 갯수
-        shuffle (bool): 섞을 것인지 
+        shuffle (bool): 섞을 것인지
         seed (int): Random seed
 
     Returns:
         folds (Generator): (train idx, valid idx) 쌍을 생성
     """
     folds = StratifiedKFold(
-        n_splits=n_splits,
-        shuffle=shuffle,
-        random_state=seed
+        n_splits=n_splits, shuffle=shuffle, random_state=seed
     ).split(np.arange(raw_train.shape[0]), raw_train_classes)
     return folds
 
