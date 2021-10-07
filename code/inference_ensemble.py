e715dc9c05ac58c0a15a7e5fd9396d9d114b13a8
tttangmin <ja036@naver.com>
2021-10-07 03:19:12 +0000

    ADD : ensemble ver

---
+++ code/inference_ensemble.py
@@ -0,0 +1,359 @@
+from sklearn import ensemble
+from transformers import (
+    AutoTokenizer,
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    Trainer,
+    TrainingArguments,
+)
+from torch.utils.data import DataLoader
+from load_data import *
+import pandas as pd
+import torch
+import torch.nn.functional as F
+
+import pickle as pickle
+import numpy as np
+import argparse
+from tqdm import tqdm
+from sklearn.metrics import classification_report
+import os
+from train import label_to_num
+import re
+
+
+def inference(model, tokenized_sent, device):
+    """
+    test dataset을 DataLoader로 만들어 준 후,
+    batch_size로 나눠 model이 예측 합니다.
+    """
+    dataloader = DataLoader(tokenized_sent, batch_size=16, shuffle=False, num_workers=4)
+    model.eval()
+    output_pred = []
+    output_prob = []
+    for i, data in enumerate(tqdm(dataloader)):
+        with torch.no_grad():
+            outputs = model(
+                input_ids=data["input_ids"].to(device),
+                attention_mask=data["attention_mask"].to(device),
+                # token_type_ids=data['token_type_ids'].to(device) # klue/roberta 관련 모델의 경우, 주석 처리
+            )
+        logits = outputs[0]
+        prob = F.softmax(logits, dim=-1).detach().cpu().numpy()
+        logits = logits.detach().cpu().numpy()
+        result = np.argmax(logits, axis=-1)
+
+        output_pred.append(result)
+        output_prob.append(prob)
+
+    return (
+        np.concatenate(output_pred).tolist(),
+        np.concatenate(output_prob, axis=0).tolist(),
+    )
+
+
+def num_to_label(label):
+    """
+    숫자로 되어 있던 class를 원본 문자열 라벨로 변환 합니다.
+    """
+    origin_label = []
+
+    # open pickle file
+    with open("dict_num_to_label.pkl", "rb") as f:
+        dict_num_to_label = pickle.load(f)
+    for v in label:
+        origin_label.append(dict_num_to_label[v])
+
+    return origin_label
+
+
+def load_test_dataset(dataset_dir, tokenizer, token_type, model):
+    """
+    test dataset을 불러온 후,
+    tokenizing 합니다.
+    """
+    test_dataset = load_data(dataset_dir)
+    # test_dataset = load_test_data(dataset_dir)
+    test_label = list(map(int, test_dataset["label"].values))
+    tokenized_test = tokenized_dataset(test_dataset, tokenizer, token_type, model)
+    return test_dataset["id"], tokenized_test, test_label
+
+
+def custom_load_test_dataset(dataset_dir, tokenizer):
+    """
+    test dataset을 불러온 후,
+    tokenizing 합니다.
+    """
+    test_dataset = load_test_data(dataset_dir)
+    test_label = list(map(int, label_to_num(test_dataset["label"].values)))
+    tokenized_test = tokenized_dataset(test_dataset, tokenizer)
+    return test_dataset["id"], tokenized_test, test_label
+
+
+def main(args):
+    """
+    주어진 dataset csv 파일과 같은 형태일 경우 inference 가능한 코드입니다.
+    """
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # load tokenizer
+    # Tokenizer_NAME = "klue/bert-base"
+
+    # load test datset
+    test_dataset_dir = "/opt/ml/dataset/test/test_data.csv"
+    # test_id, test_dataset, test_label = load_test_dataset(test_dataset_dir, tokenizer)
+    # Re_test_dataset = RE_Dataset(test_dataset, test_label)
+
+    # if args.use_data_helper:
+    #     # load custom test dataset
+    #     custom_test_dataset_dir = "/opt/ml/dataset/train/custom_test.csv"
+    #     (
+    #         custom_test_id,
+    #         custom_test_dataset,
+    #         custom_test_label,
+    #     ) = custom_load_test_dataset(custom_test_dataset_dir, tokenizer)
+    #     custom_Re_test_dataset = RE_Dataset(custom_test_dataset, custom_test_label)
+
+    if args.ensemble:
+        # ensemble 폴더에 어셈블할 데이터를 폴더채로 넣으시면 됩니다
+        output_probs = []
+        custom_output_probs = []
+        ensemble_dir = "./ensemble"
+        for folder in os.listdir(ensemble_dir):
+            # arc_name = re.match("\[klue-.+\]",folder)
+            print(folder)
+            arc_name = folder.split(" ")[0][1:-1]
+            if "klue" in arc_name:
+                arc_name = (
+                    arc_name.split("-")[0] + "/" + "-".join(arc_name.split("-")[1:])
+                )
+
+                print(f"arc_name : {arc_name}")
+
+                if "bert-" in arc_name:
+                    Tokenizer_NAME = arc_name
+                    tokenizer = AutoTokenizer.from_pretrained(Tokenizer_NAME)
+                    test_id, test_dataset, test_label = load_test_dataset(
+                        test_dataset_dir, tokenizer, folder.split(" ")[-1], "bert"
+                    )
+                else:
+                    Tokenizer_NAME = arc_name
+                    tokenizer = AutoTokenizer.from_pretrained(Tokenizer_NAME)
+                    test_id, test_dataset, test_label = load_test_dataset(
+                        test_dataset_dir, tokenizer, folder.split(" ")[-1], "robert"
+                    )
+
+            elif "xlm" in arc_name:
+                Tokenizer_NAME = arc_name
+                tokenizer = AutoTokenizer.from_pretrained(Tokenizer_NAME)
+                test_id, test_dataset, test_label = load_test_dataset(
+                    test_dataset_dir, tokenizer, folder.split(" ")[-1], "xlm"
+                )
+            else:
+                Tokenizer_NAME = arc_name
+                tokenizer = AutoTokenizer.from_pretrained(Tokenizer_NAME)
+                test_id, test_dataset, test_label = load_test_dataset(
+                    test_dataset_dir, tokenizer, folder.split(" ")[-1], ""
+                )
+                # arc_name = re.match("\[klue-.+\]",folder)
+                # arc_name_str = arc_name.group()
+            Re_test_dataset = RE_Dataset(test_dataset, test_label)
+            model = AutoModelForSequenceClassification.from_pretrained(
+                os.path.join(ensemble_dir, folder)
+            )
+            model.parameters
+            model.to(device)
+            # predict answer
+            pred_answer, output_prob = inference(
+                model, Re_test_dataset, device
+            )  # model에서 class 추론
+            output_probs.append(np.array(output_prob))
+            # if args.use_data_helper:
+            #     custom_pred_answer, custom_output_prob = inference(
+            #         model, custom_Re_test_dataset, device
+            #     )  # model에서 class 추론
+            #     custom_output_probs.append(np.array(custom_output_prob))
+
+        if args.ensemble_option == "mean":
+            mean_prob = np.mean(output_probs, axis=0)
+            pred_answer = np.argmax(mean_prob, axis=-1)
+            # if args.use_data_helper:
+            #     custom_mean_prob = np.mean(custom_output_probs, axis=0)
+            #     custom_pred_answer = np.argmax(custom_mean_prob, axis=-1)
+
+        # elif args.ensemble_option == "hard":
+        #     mean_prob = np.mean(output_probs, axis=0)
+        #     pred_answer = np.argmax(mean_prob, axis=-1)
+        #     if args.use_data_helper:
+        #         custom_mean_prob = np.mean(custom_output_probs, axis=0)
+        #         custom_pred_answer = np.argmax(custom_mean_prob, axis=-1)
+
+        # custom predict answer
+        # 숫자로 된 class를 원래 문자열 라벨로 변환.
+        pred_answer = num_to_label(pred_answer)  # 숫자로 된 class를 원래 문자열 라벨로 변환.
+        # custom_pred_answer = num_to_label(custom_pred_answer)
+
+    # else:
+    # # load my model
+    # MODEL_NAME = args.model_dir  # model dir.
+    # model = AutoModelForSequenceClassification.from_pretrained(args.model_dir)
+    # model.parameters
+    # model.to(device)
+    # # predict answer
+    # pred_answer, output_prob = inference(
+    #     model, Re_test_dataset, device
+    # )  # model에서 class 추론
+    # pred_answer = num_to_label(pred_answer)  # 숫자로 된 class를 원래 문자열 라벨로 변환.
+    # if args.use_data_helper:
+    #     # custom predict answer
+    #     custom_pred_answer, custom_output_prob = inference(
+    #         model, custom_Re_test_dataset, device
+    #     )  # model에서 class 추론
+    # 숫자로 된 class를 원래 문자열 라벨로 변환.
+    # custom_pred_answer = num_to_label(custom_pred_answer)
+
+    # make csv file with predicted answer
+    #########################################################
+    # 아래 directory와 columns의 형태는 지켜주시기 바랍니다.
+    output = pd.DataFrame(
+        {"id": test_id, "pred_label": pred_answer, "probs": output_prob}
+    )
+
+    # 최종적으로 완성된 예측한 라벨 csv 파일 형태로 저장.
+    output.to_csv("./prediction/submission_ensemble.csv", index=False)
+    #### 필수!! ##############################################
+    # if args.use_data_helper:
+    #     print(classification_report(custom_test_label, custom_pred_answer))
+    print("---- Finish! ----")
+
+
+# def main(args):
+#     """
+#     주어진 dataset csv 파일과 같은 형태일 경우 inference 가능한 코드입니다.
+#     """
+#     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#     # load tokenizer
+#     # Tokenizer_NAME = "klue/bert-base"
+#     Tokenizer_NAME = args.model
+#     tokenizer = AutoTokenizer.from_pretrained(Tokenizer_NAME)
+
+#     # load test datset
+#     test_dataset_dir = "/opt/ml/dataset/test/test_data.csv"
+#     test_id, test_dataset, test_label = load_test_dataset(test_dataset_dir, tokenizer)
+#     Re_test_dataset = RE_Dataset(test_dataset, test_label)
+
+#     if args.use_data_helper:
+#         # load custom test dataset
+#         custom_test_dataset_dir = "/opt/ml/dataset/train/custom_test.csv"
+#         (
+#             custom_test_id,
+#             custom_test_dataset,
+#             custom_test_label,
+#         ) = custom_load_test_dataset(custom_test_dataset_dir, tokenizer)
+#         custom_Re_test_dataset = RE_Dataset(custom_test_dataset, custom_test_label)
+
+#     if args.ensemble:
+#         # ensemble 폴더에 어셈블할 데이터를 폴더채로 넣으시면 됩니다
+#         output_probs = []
+#         custom_output_probs = []
+#         ensemble_dir = "./ensemble"
+#         for folder in os.listdir(ensemble_dir):
+#             model = AutoModelForSequenceClassification.from_pretrained(
+#                 os.path.join(ensemble_dir, folder)
+#             )
+#             model.parameters
+#             model.to(device)
+#             # predict answer
+#             pred_answer, output_prob = inference(
+#                 model, Re_test_dataset, device
+#             )  # model에서 class 추론
+#             output_probs.append(np.array(output_prob))
+#             if args.use_data_helper:
+#                 custom_pred_answer, custom_output_prob = inference(
+#                     model, custom_Re_test_dataset, device
+#                 )  # model에서 class 추론
+#                 custom_output_probs.append(np.array(custom_output_prob))
+
+#         if args.ensemble_option == "mean":
+#             mean_prob = np.mean(output_probs, axis=0)
+#             pred_answer = np.argmax(mean_prob, axis=-1)
+#             if args.use_data_helper:
+#                 custom_mean_prob = np.mean(custom_output_probs, axis=0)
+#                 custom_pred_answer = np.argmax(custom_mean_prob, axis=-1)
+
+#         # elif args.ensemble_option == "hard":
+#         #     mean_prob = np.mean(output_probs, axis=0)
+#         #     pred_answer = np.argmax(mean_prob, axis=-1)
+#         #     if args.use_data_helper:
+#         #         custom_mean_prob = np.mean(custom_output_probs, axis=0)
+#         #         custom_pred_answer = np.argmax(custom_mean_prob, axis=-1)
+
+#         # custom predict answer
+#         # 숫자로 된 class를 원래 문자열 라벨로 변환.
+#         pred_answer = num_to_label(pred_answer)  # 숫자로 된 class를 원래 문자열 라벨로 변환.
+#         # custom_pred_answer = num_to_label(custom_pred_answer)
+
+#     else:
+#         # load my model
+#         MODEL_NAME = args.model_dir  # model dir.
+#         model = AutoModelForSequenceClassification.from_pretrained(args.model_dir)
+#         model.parameters
+#         model.to(device)
+#         # predict answer
+#         pred_answer, output_prob = inference(
+#             model, Re_test_dataset, device
+#         )  # model에서 class 추론
+#         pred_answer = num_to_label(pred_answer)  # 숫자로 된 class를 원래 문자열 라벨로 변환.
+#         if args.use_data_helper:
+#             # custom predict answer
+#             custom_pred_answer, custom_output_prob = inference(
+#                 model, custom_Re_test_dataset, device
+#             )  # model에서 class 추론
+#         # 숫자로 된 class를 원래 문자열 라벨로 변환.
+#         # custom_pred_answer = num_to_label(custom_pred_answer)
+
+#     # make csv file with predicted answer
+#     #########################################################
+#     # 아래 directory와 columns의 형태는 지켜주시기 바랍니다.
+#     output = pd.DataFrame(
+#         {"id": test_id, "pred_label": pred_answer, "probs": output_prob}
+#     )
+
+#     # 최종적으로 완성된 예측한 라벨 csv 파일 형태로 저장.
+#     output.to_csv("./prediction/submission.csv", index=False)
+#     #### 필수!! ##############################################
+#     if args.use_data_helper:
+#         print(classification_report(custom_test_label, custom_pred_answer))
+#     print("---- Finish! ----")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # model dir
+    parser.add_argument(
+        "--model_dir", type=str, default="./best_model", help="defualt : ./best_model"
+    )
+    parser.add_argument("--model", type=str, default="klue/roberta-small")
+    parser.add_argument("--ensemble", type=bool, default=True)
+    parser.add_argument(
+        "--ensemble_option",
+        type=str,
+        default="mean",
+        help="mean, hard_voting  (default : mean)",
+    )
+    parser.add_argument(
+        "--use_data_helper",
+        type=bool,
+        default=False,
+        help="Whether to use data helper (default : False)",
+    )
+
+    args = parser.parse_args()
+    print(args)
+    main(args)
+
+"""
+ensemble 폴더에 앙상블 할 각 모델이 담긴 폴더의 형식을 
+[모델명] token_type
+로 통일 해주시면 됩니다.
+"""
