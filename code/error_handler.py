3a19f8f4445176c5685ffcffb08e46e730587463
박진영 <nazzang49@naver.com>
2021-10-09 16:34:17 +0900

    UPDATE : docstring 추가

--- code/error_handler.py
+++ code/error_handler.py
@@ -1,32 +1,33 @@
 class ErrorCode():
     '''
-    에러 코드 관리
+    A constant class for managing error codes
     '''
     NOT_FOUND = '404'
     BAD_REQUEST = '400'
 
 class ErrorMsg():
     '''
-    에러 메시지 관리
+    A constant class for managing error messages
     '''
-    WRONG_INPUT = '입력이 잘못되었습니다.'
+    WRONG_INPUT = '입력값 형태가 올바르지 않습니다.'
+    ALREADY_EXIST = '이미 존재하는 명칭입니다.'
 
 class CustomError(Exception):
     '''
-    커스텀 에러 코드 클래스
+    A error class for customized error code and messages
+
+    Args:
+        code (str): A customized error code
+        msg (str): A customized error messages
     '''
     def __init__(self, code, msg):
-        self.code = code # from ErrorCode
-        self.msg = msg # from ErrorMsg
+        self.code = code    # from ErrorCode class
+        self.msg = msg      # from ErrorMsg class
 
     def __str__(self):
-        '''
-        에러 메시지 출력부
-        '''
         return f'[ {self.code} ] {self.msg}'
 
-
-# 예시
+# example
 tmp = None
 try:
     if tmp is None:
